f6b6f1bbe23a8464bd53b28ddf3b18515f0dcc44
Michael Deli <31163949+MichaelODeli@users.noreply.github.com>
2020-07-12 17:17:59 +0500

    Template for future updates
    Templates and separate plugins were created for each team in the application. Subsequently, additional plugins will be compiled and closed for viewing and editing.

---
+++ mine_modanalyzer.py
@@ -0,0 +1,59 @@
+import os
+from mine_modanalyzer_res import base
+from mine_modanalyzer_res import minefunc
+from mine_modanalyzer_res import modfunc
+os.system("cls")
+base.launcher()
+base.welcome()
+e = 1
+
+while e==1:
+    console_input=[ ]
+    console_input = list(map(str,input('>>> ').split()))
+    if console_input[0]=="help":
+        print("In develop.")
+
+    elif console_input[0]=="q" or console_input[0]=="exit":
+        break
+
+    elif console_input[0]=="mine":
+        if console_input[1]=="install":
+            print("Feature in development.")
+        elif console_input[1]=="setcategory":
+            print("Feature in development.")
+        elif console_input[1]=="launch":
+            print("Feature in development.")
+        elif console_input[1]=="setdirectory":
+            if console_input[2]=="game":
+                print("Feature in development.")
+            if console_input[2]=="mod":
+                print("Feature in development.")
+            if console_input[2]=="backup":
+                print("Feature in development.")
+
+    elif console_input[0]=="mod":
+        if console_input[1]=="name":
+            print("Feature in development.")
+        if console_input[1]=="install":
+            print("Feature in development.")
+        if console_input[1]=="remove":
+            print("Feature in development.")
+        if console_input[1]=="disable":
+            print("Feature in development.")
+        if console_input[1]=="enable":
+            print("Feature in development.")
+        if console_input[1]=="info":
+            print("Feature in development.")
+        if console_input[1]=="backup":
+            print("Feature in development.")
+        if console_input[1]=="check":
+            print("Feature in development.")
+        if console_input[1]=="update":
+            print("Feature in development.")
+        if console_input[1]=="list":
+            print("Feature in development.")
+
+    
+
+    else:
+        print("Unknown command. Try again or type 'info'.")
---
+++ mine_modanalyzer_res/base.py
@@ -0,0 +1,25 @@
+import random
+import os
+import time
+verfile=open('ver.txt', 'r')
+ver=verfile.read()
+verfile.close
+def launcher():
+    i=1
+    randomint=1
+    while i<6:
+        # В будущем тут будет цикл for и будем анализировать каждый файл из папки с модификациями.
+        # Пока что, тут рандомный тикер.
+        print('Please, be patient! Program is loading...')
+        x='####'
+        x=x*i
+        print(x)
+        randomint=random.randint(1,3)
+        time.sleep(randomint)
+        i+=1
+        os.system("cls")
+    print("Program is loaded")
+
+def welcome():
+    print("Minecraft Mod Analyzer "+ver)
+    print("To get help, print here 'help'.")
