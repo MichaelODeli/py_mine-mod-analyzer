63e63135dca573971d0ce6a04ae2b0eeebd7ec3c
Michael Deli <31163949+MichaelODeli@users.noreply.github.com>
2020-07-13 16:02:50 +0500

    Added new command
    > Added mod list command
    > Disabled Loader

--- mine_modanalyzer.py
+++ mine_modanalyzer.py
@@ -2,14 +2,15 @@ import os
 from mine_modanalyzer_res import base
 from mine_modanalyzer_res import minefunc
 from mine_modanalyzer_res import modfunc
+# выносим все в единый py-файл, это должно повлиять на работоспособность и не надо будет тянуть кучу файлов
 os.system("cls")
-base.launcher()
+# base.launcher()
 base.welcome()
 e = 1
 
 while e==1:
     console_input=[ ]
-    console_input = list(map(str,input('>>> ').split()))
+    console_input = list(map(str,input(' >>>  ').split()))
     if console_input[0]=="help":
         print("You can view additional information on github. https://github.com/MichaelODeli/py_mine-mod-analyzer")
 
@@ -26,35 +27,41 @@ while e==1:
         elif console_input[1]=="setdirectory":
             if console_input[2]=="game":
                 print("Feature in development.")
-            if console_input[2]=="mod":
+            elif console_input[2]=="mod":
                 print("Feature in development.")
-            if console_input[2]=="backup":
+            elif console_input[2]=="backup":
                 print("Feature in development.")
 
     elif console_input[0]=="mod":
         if console_input[1]=="name":
             print("Feature in development.")
-        if console_input[1]=="install":
+        elif console_input[1]=="install":
             print("Feature in development.")
-        if console_input[1]=="remove":
+        elif console_input[1]=="remove":
             print("Feature in development.")
-        if console_input[1]=="disable":
+        elif console_input[1]=="disable":
             print("Feature in development.")
-        if console_input[1]=="enable":
+        elif console_input[1]=="enable":
             print("Feature in development.")
-        if console_input[1]=="info":
+        elif console_input[1]=="info":
             print("Feature in development.")
-        if console_input[1]=="backup":
+        elif console_input[1]=="backup":
             print("Feature in development.")
-        if console_input[1]=="check":
+        elif console_input[1]=="check":
             print("Feature in development.")
-        if console_input[1]=="update":
+        elif console_input[1]=="update":
             print("Feature in development.")
-        if console_input[1]=="list":
-            print("Feature in development.")
+        elif console_input[1]=="list":
+            try:
+                if console_input[2]=="disable":
+                    modfunc.listmoddis()
+                elif console_input[2]=="enable":
+                    modfunc.listmodena()
+            except:
+                modfunc.listmodall()
 
     
 
     else:
         print("Unknown command. Try again or type 'info'.")
-input()
+input("Press Enter to exit ")
--- mine_modanalyzer_res/modfunc.py
+++ mine_modanalyzer_res/modfunc.py
@@ -1,2 +1,26 @@
-print("Nothing")
-# тут будут функции для команд mod
+import os
+import fnmatch
+def listmodall():
+    print("All mods:")
+    apdir=os.getenv('APPDATA')
+    listOfFiles = os.listdir(apdir+r"\.minecraft\mods")  
+    pattern = "*.jar"  
+    for entry in listOfFiles:  
+        if fnmatch.fnmatch(entry, pattern):
+            print(entry)
+def listmoddis():
+    print("Disabled mods:")
+    apdir=os.getenv('APPDATA')
+    listOfFiles = os.listdir(apdir+r"\.minecraft\mods")  
+    pattern = "*.jar_backup"  
+    for entry in listOfFiles:  
+        if fnmatch.fnmatch(entry, pattern):
+            print(entry)
+def listmodena():
+    print("Enabled mods:")
+    apdir=os.getenv('APPDATA')
+    listOfFiles = os.listdir(apdir+r"\.minecraft\mods")  
+    pattern = "*.jar*"  
+    for entry in listOfFiles:  
+        if fnmatch.fnmatch(entry, pattern):
+            print(entry)
